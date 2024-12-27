03de2a4765e5b90f0c13db28675e6169c8adfe81
simeon2941 <simeonlico@gmail.com>
2024-12-26 21:08:58 -0600

    add github actions

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/data-eng-learning/", // Add this for GitHub Pages
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-})
+});
