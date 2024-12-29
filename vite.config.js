fff47a6a9b419630bda84a78ae6aacc2b7a1b3fd
simeon2941 <simeonlico@gmail.com>
2024-12-28 21:27:05 -0600

    try ths

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ["react", "react-dom", "recharts", "lucide-react"],
-          examples: ["/src/examples/**/*.jsx"],
         },
       },
     },
